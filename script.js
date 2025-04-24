a797dc0244e8a84070583220ad2d4694c27a96ef
fmholder <193525232+fmholder@users.noreply.github.comgit config user.email 193525232+fmholder@users.noreply.github.com>
2025-04-23 17:33:46 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,31 @@
+function showDescription(id) {
+    let st = "string";
+    let image = "path";
+    
+    switch(id) {
+        case 1 :
+            st = "I had to build a Node.js console app that generates random messages each time a user runs the program and log the version project with Git/GitHub. The theme was, 'What should I make for dinner tonight?'"
+            imageScript = "resources/kikki.ico";
+            break;
+        case 2 :
+            st = "Description for box 2 - Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime, saepe sint. Numquam ad labore quos maiores mollitia ea magni consectetur?"
+            imageScript = "/resources/images/kikki portrait.jpg";
+            break;
+        case 3 :
+            st = "Description for box 3 - Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime, saepe sint. Numquam ad labore quos maiores mollitia ea magni consectetur?"
+            imageScript = "resources/kikki.ico";
+            break;
+        case 4 :
+            st = "Description for box 4 - Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime, saepe sint. Numquam ad labore quos maiores mollitia ea magni consectetur?"
+            imageScript = "resources/kikki.ico";
+            break;
+        case 5 :
+            st = "Description for box 5 - Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime, saepe sint. Numquam ad labore quos maiores mollitia ea magni consectetur?"
+            imageScript = "resources/kikki.ico";
+            break;
+        default :  
+            st = "---";
+    }// switch
+    document.getElementById('descBox').innerHTML = st;
+    document.getElementById('projectImage').src= imageScript;
+}// function
