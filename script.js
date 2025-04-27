cca3ee23b0519e6d88f3630ba7a8010aebcf59aa
fmholder <193525232+fmholder@users.noreply.github.com>
2025-04-27 08:37:25 -0700

    changed responsive to a menu for small screens

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ function showDescription(id) {
             break;
         case 2 :
             st = "Description for box 2 - Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime, saepe sint. Numquam ad labore quos maiores mollitia ea magni consectetur?"
-            imageScript = "/resources/images/kikki portrait.jpg";
+            imageScript = "./resources/images/kikki portrait.jpg";
             break;
         case 3 :
             st = "Description for box 3 - Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime, saepe sint. Numquam ad labore quos maiores mollitia ea magni consectetur?"
@@ -28,4 +28,48 @@ function showDescription(id) {
     }// switch
     document.getElementById('descBox').innerHTML = st;
     document.getElementById('projectImage').src= imageScript;
+    document.getElementsByClassName(mobile-description).innerHTML = st;
 }// function
+
+// below is from Conding2GO https://www.youtube.com/watch?v=m7YDWNz65iI
+const openButton = document.getElementById('open-sidebar-button')
+const navbar = document.getElementById('navbar')
+
+const media = window.matchMedia("(width < 700px)")
+
+media.addEventListener('change', (e) => updateNavbar(e))
+
+function updateNavbar(e){
+  const isMobile = e.matches
+  console.log(isMobile)
+  if(isMobile){
+    navbar.setAttribute('inert', '')
+  }
+  else{
+    // desktop device
+    navbar.removeAttribute('inert')
+  }
+}
+
+function openSidebar(){
+  navbar.classList.add('show')
+  openButton.setAttribute('aria-expanded', 'true')
+  navbar.removeAttribute('inert')
+}
+
+function closeSidebar(){
+  navbar.classList.remove('show')
+  openButton.setAttribute('aria-expanded', 'false')
+  navbar.setAttribute('inert', '')
+}
+
+// For Bookmark Links
+// const navLinks = document.querySelectorAll('nav a')
+// navLinks.forEach(link => {
+//   link.addEventListener('click', () => {
+//     closeSidebar()
+//   })
+// })
+
+updateNavbar(media)
+
